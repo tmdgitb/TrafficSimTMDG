bfccaa8b554f148e68d0ccc7311ece19252707f1
Ilham <myung1012@gmail.com>
2016-10-05 19:26:14 +0700

    Change findVehicleInBehind method, add prevSegment & nextSegment to RoadSegment, split up the OSM Loading to fasten the load time

---
+++ OSMConverter/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OSMConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LoadButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 86);
+            this.panel1.TabIndex = 0;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.LoadButton.Location = new System.Drawing.Point(88, 33);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load OSM";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 104);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 74);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(0, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please load the OSM File to Convert";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 189);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "OSM Converter for SimTMDG";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OSMConverter/Form1.cs
@@ -0,0 +1,214 @@
+﻿using SimTMDG.LoadingForm;
+using SimTMDG.Road;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OSMConverter
+{
+    public partial class Form1 : Form
+    {
+
+        double minLon;
+        double maxLon;
+        double minLat;
+        double maxLat;
+        Boolean boundsDefined = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Load File
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = Application.ExecutablePath;
+                ofd.AddExtension = true;
+                ofd.DefaultExt = @".xml";
+                ofd.Filter = @"OpenStreetMap|*.osm";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    LoadOsmMap(ofd.FileName);
+                }
+            }
+            #endregion
+
+
+
+            label1.Text = "File has been successfully converted. abcdefg lorem ipsum dolor sit amet";
+        }
+
+
+        private void LoadOsmMap(string path)
+        {
+            LoadingForm lf = new LoadingForm();
+            lf.Text = "Loading file '" + path + "'...";
+            lf.Show();
+
+            lf.SetupUpperProgress("Loading Document...", 5);
+
+            XmlDocument xd = new XmlDocument();
+            xd.Load(path);
+
+            XmlNode mainNode = xd.SelectSingleNode("//osm");
+            XmlNode bounds = xd.SelectSingleNode("//osm/bounds");
+
+            if (bounds == null)
+            {
+                Debug.WriteLine("bounds null");
+            }
+            else
+            {
+                XmlNode minLonNode = bounds.Attributes.GetNamedItem("minlon");
+                XmlNode maxLonNode = bounds.Attributes.GetNamedItem("maxlon");
+                XmlNode minLatNode = bounds.Attributes.GetNamedItem("minlat");
+                XmlNode maxLatNode = bounds.Attributes.GetNamedItem("maxlat");
+
+                if (minLonNode != null)
+                {
+                    minLon = double.Parse(minLonNode.Value, CultureInfo.InvariantCulture);
+                    boundsDefined = true;
+                }
+                else { minLon = 0; }
+
+                if (maxLonNode != null)
+                {
+                    maxLon = double.Parse(maxLonNode.Value, CultureInfo.InvariantCulture);
+                    boundsDefined = true;
+                }
+                else { maxLon = 0; }
+
+                if (minLatNode != null)
+                {
+                    minLat = double.Parse(minLatNode.Value, CultureInfo.InvariantCulture);
+                    boundsDefined = true;
+                }
+                else { minLat = 0; }
+
+                if (maxLatNode != null)
+                {
+                    maxLat = double.Parse(maxLatNode.Value, CultureInfo.InvariantCulture);
+                    boundsDefined = true;
+                }
+                else { maxLat = 0; }
+
+                Debug.WriteLine("minLong maxLat: " + minLon + ", " + maxLat);
+
+
+                lf.StepUpperProgress("Parsing Nodes...");
+                XmlNodeList xnlLineNode = xd.SelectNodes("//osm/node");
+                lf.SetupLowerProgress("Parsing Nodes", xnlLineNode.Count - 1);
+
+                Stopwatch sw = Stopwatch.StartNew();
+                foreach (XmlNode aXmlNode in xnlLineNode)
+                {
+                    // Node in einen TextReader packen
+                    TextReader tr = new StringReader(aXmlNode.OuterXml);
+                    // und Deserializen
+                    XmlSerializer xs = new XmlSerializer(typeof(Node));
+                    Node n = (Node)xs.Deserialize(tr);
+                    n.latLonToPos(minLon, maxLat);
+
+                    // ab in die Liste
+                    nc._nodes.Add(n);
+
+                    lf.StepLowerProgress();
+                }
+                sw.Stop();
+                Console.WriteLine("Total query time: {0} ms", sw.ElapsedMilliseconds);
+
+
+                lf.StepUpperProgress("Parsing Ways / Roads...");
+                XmlNodeList xnlWayNode = xd.SelectNodes("//osm/way");
+                lf.SetupLowerProgress("Parsing Ways", xnlWayNode.Count - 1);
+
+                sw = Stopwatch.StartNew();
+                foreach (XmlNode aXmlNode in xnlWayNode)
+                //Parallel.ForEach(xnlWayNode, (XmlNode aXmlNode) =>
+                {
+                    XmlNodeList nds = aXmlNode.SelectNodes("nd");
+                    XmlNode onewayTag = aXmlNode.SelectSingleNode("tag[@k='oneway']");
+                    XmlNode highwayTag = aXmlNode.SelectSingleNode("tag[@k='highway']");
+                    XmlNode numlanesTag = aXmlNode.SelectSingleNode("tag[@k='lanes']");
+
+                    List<XmlNode> lnd = new List<XmlNode>();
+
+                    foreach (XmlNode nd in nds)
+                    {
+                        lnd.Add(nd);
+                    }
+
+                    if (onewayTag != null)
+                    {
+                        string oneway = onewayTag.Attributes.GetNamedItem("v").Value;
+
+                        //if (oneway == "-1")
+                        //{
+                        //    makeWaySegment_old(lnd, highwayTag, numlanesTag, oneway);
+                        //}
+                        //else
+                        //{
+                        makeWaySegment_old(lnd, highwayTag, numlanesTag, oneway);
+                        //}
+                    }
+                    else
+                    {
+                        makeWaySegment_old(lnd, highwayTag, numlanesTag, "");
+                    }
+
+                    lf.StepLowerProgress();
+
+                }
+
+                lf.StepUpperProgress("Search segment connection...");
+                lf.SetupLowerProgress("Search segment connectio", nc.segments.Count - 1);
+
+                for (int i = 0; i < nc.segments.Count; i++)
+                {
+                    nc.segments[i].nextSegment = nc.segments.FindAll(x => x.startNode == nc.segments[i].endNode);
+                    nc.segments[i].prevSegment = nc.segments.FindAll(x => x.endNode == nc.segments[i].startNode);
+
+                    lf.StepLowerProgress();
+                }
+
+                //});
+                sw.Stop();
+                Console.WriteLine("Total query time: {0} ms", sw.ElapsedMilliseconds);
+
+                #region manually generate route
+
+                lf.StepUpperProgress("Manually Generate Routes...");
+
+                //Debug.WriteLine("Segment Count" + nc.segments.Count);
+                //manuallyAddRoute();
+
+                lf.StepUpperProgress("Done");
+                lf.ShowLog();
+
+                lf.Close();
+                lf = null;
+
+
+
+                #endregion
+            }
+        }
+    }
+}
